d6cbc02a1f8c91f512a37843f104b7eb62f84c3e
codeagles <hcn0521@outlook.com>
2021-05-03 10:27:00 +0800

    增加ProtoBuf的demo测试

---
+++ src/test/java/TestProtoBuf.java
@@ -0,0 +1,47 @@
+import com.bfxy.netty.protobuf.UserModule;
+import com.google.protobuf.InvalidProtocolBufferException;
+
+import java.util.Arrays;
+
+/**
+ * Created with IntelliJ IDEA.
+ * User: Codeagles
+ * Date: 2021/5/3
+ * Time: 上午10:18
+ * <p>
+ * Description:
+ */
+public class TestProtoBuf {
+
+    public static byte[] serialObject2Bytes(){
+        UserModule.User.Builder builder = UserModule.User.newBuilder();
+        builder.setUserId("1001")
+                .setAge(30)
+                .setUserName("臧三")
+                .addFavorite("足球")
+                .addFavorite("篮球");
+
+        UserModule.User user = builder.build();
+        byte[] data = user.toByteArray();
+        System.out.println(Arrays.toString(data));
+        return data;
+    }
+
+
+    public static UserModule.User serialBytes2Object(byte[] data){
+        try {
+            return UserModule.User.parseFrom(data);
+        } catch (InvalidProtocolBufferException e) {
+            e.printStackTrace();
+        }
+        return null;
+    }
+
+    public static void main(String[] args) {
+        byte[] bytes = serialObject2Bytes();
+        UserModule.User user = serialBytes2Object(bytes);
+
+        System.out.println("UserId:"+ user.getUserId());
+
+    }
+}
